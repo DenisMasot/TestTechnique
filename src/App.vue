103f48e1e7fa7cbde4f0c855e3aa4bdbbc361e38
DenisMasot <denis.masot@gmail.com>
2018-09-30 10:07:30 +0200

    add material design + custom card WIP

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,12 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
+
 export default {
   name: 'App'
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="hello">
+  <div class="home">
 
     <select v-model="selected">
+      <option disabled value="">Please select one</option>
       <option v-for="article in datas.articles" :key="article.id">{{article.source.name}}</option>
     </select>
-    <!-- {{datas.articles}}  -->
 
-    <div>Selected: {{ selected }}</div>
+        <section class="md-card md-theme-demo-lightr" v-for="article in datas.articles" v-if="article.source.name === selected" :key="article.id">
+          <md-card-header >
+            <img :src="article.urlToImage" :alt="article.title">
+            <div class="md-title">{{article.title}}</div>
+          </md-card-header>
 
+          <md-card-content>
+            {{article.description}}
+          </md-card-content>
+
+          <md-card-actions>
+            <md-button :href="article.url" target="_blank">see more</md-button>
+          </md-card-actions>
+        </section>
   </div>
 </template>
 
@@ -36,6 +48,22 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="scss" scoped>
+.home {
+    .md-card {
+      width: 320px;
+      margin: 4px;
+      display: inline-block;
+      vertical-align: top;
+      .md-card-header{
+        padding: 0;
+        &>img {
+          border-radius: 0;
+        }
+      }
+      .md-card-content, .md-card-actions{
+        display: none;
+      }
+    }
+}
 </style>
